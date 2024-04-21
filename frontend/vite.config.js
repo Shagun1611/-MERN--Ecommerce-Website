65dba344f0f1a8377a5fc4d17f44067e92dfa19d
Shagun Sharma <shagu22222@gmail.com>
2024-04-21 12:54:22 +0530

    Third commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      // "/api/": "http://localhost:5000",
+      // "/uploads/": "http://localhost:5000",
+      "/api/": "https://mern-ecommerce-website-xthw.onrender.com",
+      "/uploads/": "https://mern-ecommerce-website-xthw.onrender.com",
     },
   },
 });
+// One Change i have done here
